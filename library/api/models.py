298793849a7976d7ef1e2d5255dd2d7d7c59ec23
Rob <rob@ubuntu.ubuntu-domain>
2018-11-02 22:15:52 -0400

    Created movie model, started writing tests for it

--- library/api/models.py
+++ library/api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    TITLE_TYPES = (
+        ('MOVIE', 'Movie'),
+        ('SHORT', 'Short'),
+        ('SERIES', 'TV Series'),
+        ('EPISODE', 'TV Episode'),
+        ('VID', 'Video')
+    )
+
+    tconst = models.CharField(max_length=10, primary_key=True, null=False, blank=False, unique=True)
+    title_type = models.CharField(max_length=15, null=False, blank=False, choices=TITLE_TYPES, db_column='titleType')
+    primary_title = models.CharField(max_length=255, null=False, blank=False, db_column='primaryTitle')
+    original_title = models.CharField(max_length=255, null=True, blank=True, db_column='originalTitle')
+    is_adult = models.BooleanField(default=False, db_column='isAdult')
+    start_year = models.IntegerField(null=True, blank=True, default=None, db_column='startYear')
+    end_year = models.IntegerField(null=True, blank=True, default=None, db_column='endYear')
+    runtime_minutes = models.IntegerField(null=False, default=0, db_column='runtimeMinutes')
+    # TODO: add genres here (foreign key)
+
+    def __str__(self):
+        return self.primary_title
+
---
+++ library/api/tests/test_models.py
@@ -0,0 +1,29 @@
+# library-api/library/api/tests/test_models.py
+
+from django.db import IntegrityError
+from django.test import TestCase
+
+from ..models import Movie
+
+
+class TestMovieModel(TestCase):
+    def setUp(self):
+        self.movie = Movie(
+            primary_title="Django Unchained", tconst='tt1853728', original_title='Django Unchained', title_type='MOVIE',
+            is_adult=False, start_year=2012, end_year=2012, runtime_minutes=165
+        )
+        self.movie.save()
+
+    def test_movie_creation(self):
+        self.assertEqual(Movie.objects.count(), 1)
+
+    def test_movie_representation(self):
+        self.assertEqual(self.movie.primary_title, str(self.movie))
+
+    def test_duplicate_id_not_allowed(self):
+        with self.assertRaises(IntegrityError):
+            Movie.objects.create(
+                primary_title="Willow", tconst='tt1853728', original_title='Willow', title_type='MOVIE',
+                is_adult=False, start_year=1988, end_year=1988, runtime_minutes=126
+            )
+
